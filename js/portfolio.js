43bec94d19fa817580b0b8a51da01e0379e93c08
Melusine Chrs <melusine.chrs@gmail.com>
2024-11-25 16:14:41 +0100

    micro baby update

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,10 +3,21 @@ const rootfolder = document.getElementById('root');
 const subfolders = document.getElementById('subfolders');
 const backfolder = document.getElementById('backend');
 const frontfolder = document.getElementById('frontend');
+const nBack = document.querySelectorAll('#list>p:not(.back)');
+const nFront = document.getElementsByClassName('front');
 
 portfolio.addEventListener('click', (e) => {
     if (e.target == rootfolder) {
         let visibility = subfolders.style.contentVisibility;
         subfolders.style.contentVisibility = (visibility == "" || visibility == "hidden") ? "visible" : "hidden";
+    }
+
+    if (e.target == backfolder) {
+        for (const p of nBack) {
+            p.style.contentVisibility = "hidden";
+        }
+        for (const b of document.getElementsByClassName('back')) {
+            b.style.contentVisibility = "visible";
+        }
     }
 })
