ca5007bedf11cebff17090d18d4385eb211ef801
Melusine Chrs <melusine.chrs@gmail.com>
2024-12-16 18:03:14 +0100

    quart fonctionnel

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,112 +1,42 @@
-const portfolio = document.getElementById('portfolio_window');
-const rootfolder = document.getElementById('root');
-const subfolders = document.getElementById('subfolders');
-const backfolder = document.getElementById('backend');
-const frontfolder = document.getElementById('frontend');
-const projectList = document.getElementById('list');
-const infoDisplay = document.getElementById('infos');
+const folders = [e_root, e_js, e_php];
+let lastSeen;
+let lastInteract;
 
-const nBack = projectList.querySelectorAll('p:not(.back)');
-const nFront = projectList.querySelectorAll('p:not(.front)');
-
-portfolio.addEventListener('click', (e) => {
-    // click folder
-    if ([rootfolder, backfolder, frontfolder].includes(e.target)) {
-
-        // effacer choix précédent
-        if (typeof folderChoice !== 'undefined') {
-            folderChoice.classList.remove('selectedDisplay');
-        }
-
-        // nouveau choix
-        folderChoice = e.target;
-        folderChoice.classList.add('selectedDisplay');
+portfolio_window.addEventListener('click', (e) => {
+    // comportement dossiers
+    if (folders.includes(e.target)) {
+        let folderContent = e.target.nextElementSibling;
+        let visibility = folderContent.style.display;
+        folderContent.style.display = (visibility == "" || visibility == "block") ? "none" : "block";
     }
 
-    if (Array.from(projectList.children).includes(e.target)) {
-
-        // effacer choix précédent
-        if (typeof projectChoice !== 'undefined') {
-            projectChoice.classList.remove('selectedDisplay');
+    // affichage détail
+    if (!folders.includes(e.target) && Array.from(explorer.getElementsByTagName('p')).includes(e.target)) {
+        if (window.innerWidth < 600) detail.style.display = "block";
+        for (const elem of detail.children) {
+            elem.style.display = "block";
+            elem.firstElementChild.style.display = "block";
         }
 
-        // nouveau choix
-        projectChoice = e.target;
-        projectChoice.classList.add('selectedDisplay');
-    }
-
-    // click "My projects"
-    if (e.target == rootfolder) {
-        
-        // (dés)-affichage subfolders
-        if (window.innerWidth > 600) {
-            let visible = subfolders.style.display;
-            subfolders.style.display = (visible == "" || visible == "none") ? "block" : "none";
-        }
-
-        // afficher projets
-        for (const p of document.querySelectorAll('#list>p')) {
-            p.style.display = "block";
-        }
-    }
-
-    // click "Backend"
-    if (e.target == backfolder) {
-        for (const p of nBack) {
-            p.style.display = "none";
-        }
-        for (const p of document.getElementsByClassName('back')) {
-            p.style.display = "block";
-        }
-    }
-
-    // click "Frontend"
-    if (e.target == frontfolder) {
-        for (const p of nFront) {
-            p.style.display = "none";
-        }
-        for (const p of document.getElementsByClassName('front')) {
-            p.style.display = "block";
-        }
-    }
-
-    // click projet
-    if (Array.from(projectList.children).includes(e.target))
-    {
-        fetch("../assets/content/content.json")
-        .then(res => res.json())
-        .then(data =>  {
-            if (window.innerWidth < 600) {
-                infoDisplay.style.display = "block";
+        if (typeof lastSeen !== "undefined") {
+            for (const elem of document.getElementsByClassName(lastSeen)) {
+                elem.style.display = "none";
             }
-            for (const elem of infoDisplay.children) {
-                if (window.innerWidth > 600) {
-                    elem.style.display = "block";
-                }
-                let contentfield = elem.lastElementChild;
-                let contentdata = data.portfolio.projects[e.target.id.substring(4)][contentfield.id.substring(2)];
+        }
+        for (const elem of document.getElementsByClassName(e.target.id)) {
+            elem.style.display = "block";
+        }
 
-                if (contentfield.id.substring(2) == "desc") {
-                    let lang = document.getElementById('lang').textContent;
-                    contentfield.textContent = contentdata[lang];
-                }
-                else if (contentfield.id.substring(2) != "links") {
-                    contentfield.textContent = contentdata;
-                } else {
+        lastSeen = e.target.id;
+    }
 
-                    contentfield.innerHTML = "";
-                    for (const k of Object.keys(contentdata)) {
-                        let nA = document.createElement('a');
-                        nA.textContent = k;
-                        nA.href = contentdata[k];
-                        contentfield.append(nA);
-                    }
-                    
-                }
-
-            }
-
-        })
+    // reset lors fermeture fenêtre
+    if (e.target.className === "close") {
+        if (window.innerWidth < 600) detail.style.display = "none";
+        for (const elem of detail.children) {
+            elem.style.display = "none";
+            elem.firstElementChild.style.display = "none";
+        }
     }
 
 })
